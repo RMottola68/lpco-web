9a54d2be14d9fd5bc68a32504caf41c9c43f638f
Ryan Mottola <rmottola68@gmail.com>
2021-07-26 21:01:59 -0400

    added state to collapse mobile menu

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Container, Navbar, Nav, NavDropdown, Button } from 'react-bootstrap';
 import LPLOGOCLEAN from './assets/LPLOGOCLEAN.png';
 import  {NavLink } from "react-router-dom";
 
-const navigation = (props) => {
+const Navigation = (props) => {
+
+    const [expanded, setExpanded] = useState(false);
+
     return (
         
-        <Navbar bg="light" expand="lg">
+        <Navbar bg="light" expand="lg" expanded={expanded}>
             <Container  className={'d-flex justify-content-between'}>
                 <Navbar.Brand href="Home" className={'col-2 col-xl-4'}>
                     <img
@@ -23,13 +26,14 @@ const navigation = (props) => {
                     <h5 className={''}>LP & COMPANY HAIR DESIGN</h5>
                 </div>
 
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Toggle onClick={() => setExpanded(expanded ? false : "expanded")} aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
+                    
                     <Nav className="col">
-                        <Nav.Link href="#home"><NavLink to={'/Home'} ><Button type={'button'} variant="outline-danger" className={' '}>Home</Button></NavLink></Nav.Link>
-                        <Nav.Link href="#services"><NavLink to={'/Services'} ><Button type={'button'}  variant="outline-danger" className={''} >Services</Button></NavLink></Nav.Link>
-                        <Nav.Link href="#community"><NavLink to={'/Community'} ><Button type={'button'}  variant="outline-danger" className={''} >Community</Button></NavLink></Nav.Link>
-                        <Nav.Link href="#contact"><NavLink to={'/Contact'} ><Button type={'button'}  variant="outline-danger" className={''} >Contact</Button></NavLink></Nav.Link>
+                        <Nav.Link href="#home" onClick={() => setExpanded(false)}><NavLink to={'/Home'} ><Button type={'button'} variant="outline-danger" className={' '}>Home</Button></NavLink></Nav.Link>
+                        <Nav.Link href="#services" onClick={() => setExpanded(false)}><NavLink to={'/Services'} ><Button type={'button'}  variant="outline-danger" className={''} >Services</Button></NavLink></Nav.Link>
+                        <Nav.Link href="#community" onClick={() => setExpanded(false)}><NavLink to={'/Community'} ><Button type={'button'}  variant="outline-danger" className={''} >Community</Button></NavLink></Nav.Link>
+                        <Nav.Link href="#contact" onClick={() => setExpanded(false)}><NavLink to={'/Contact'} ><Button type={'button'}  variant="outline-danger" className={''} >Contact</Button></NavLink></Nav.Link>
                         
                     </Nav>
                 </Navbar.Collapse>
@@ -39,4 +43,4 @@ const navigation = (props) => {
     );
 }
 
-export default navigation;
+export default Navigation;
