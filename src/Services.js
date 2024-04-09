3f5d8c35cb2182c389963c576fb70276866c36c1
RMottola68 <RMottola68@gmail.com>
2024-04-09 15:48:14 -0400

    added CC prices, need to organize grid spacing

--- src/Services.js
+++ src/Services.js
@@ -17,64 +17,64 @@ const Services = (props) => {
                         <h3 className={'text-decoration-underline list-group'}>H A I R S T Y L E</h3>
                         <p className={'my-0'}>(Includes Blowdry)</p>
                         <p className={'mt-0'}>(Prices Vary on Detailed Designs, Longer Length of Hair, and Designer's Time)</p>
-                        <h4 className={'d-flex justify-content-between list-group-item'}>Haircuts<span className={''}>$35-$75</span></h4>
-                        <h4 className={'d-flex justify-content-between list-group-item'}>Blowdry<span className={''}>$37-$55</span></h4>
-                        <h4 className={'d-flex justify-content-between text-start list-group-item'}>Children's Haircuts Under 12<span className={'my-auto align-text-start'}>$20-$25</span></h4>
-                        <h4 className={'d-flex justify-content-between list-group-item'}>Up-Do / Curling<span className={''}>$85-$150</span></h4>
-                        <h4 className={'d-flex justify-content-between list-group-item'}>Keratin Treatment<span className={''}>$103/HR</span></h4>
-                        <h4 className={'d-flex justify-content-between list-group-item'}>Body Wave / Perm <span className={''}>$75-$150</span></h4>
-                        <h4 className={'d-flex justify-content-between list-group-item'}>Special Effects & Relaxers<span className={'my-auto'}>$65-$150</span></h4>
-                        <h4 className={'d-flex justify-content-between list-group-item'}>Scalp Treatments<span className={''}>$30</span></h4>
+                        <h4 className={'d-flex justify-content-between align-center list-group-item'}>Haircuts<span className={'d-flex'}>$35<h6 className={'text-danger'}>$35.05</h6>-$75<h6 className={'text-danger'}>$77.25</h6></span></h4>
+                        <h4 className={'d-flex justify-content-between list-group-item'}>Blowdry<span className={'d-flex'}>$37<h6 className={'text-danger'}>$38.11</h6>-$55<h6 className={'text-danger'}>$56.65</h6></span></h4>
+                        <h4 className={'d-flex justify-content-between text-start list-group-item'}>Children's Haircuts Under 12<span className={'my-auto align-text-start'}>$20<h6 className={'text-danger'}>$20.6</h6>-$25<h6 className={'text-danger'}>$25.75</h6></span></h4>
+                        <h4 className={'d-flex justify-content-between list-group-item'}>Up-Do / Curling<span className={'d-flex'}>$85<h6 className={'text-danger'}>$87.55</h6>-$150<h6 className={'text-danger'}>$154.5</h6></span></h4>
+                        <h4 className={'d-flex justify-content-between list-group-item'}>Keratin Treatment<span className={'d-flex'}>$103<h6 className={'text-danger'}>$106.09</h6>/HR</span></h4>
+                        <h4 className={'d-flex justify-content-between list-group-item'}>Body Wave / Perm <span className={'d-flex'}>$75<h6 className={'text-danger'}>$77.25</h6>-$150<h6 className={'text-danger'}>$154.05</h6></span></h4>
+                        <h4 className={'d-flex justify-content-between list-group-item'}>Special Effects & Relaxers<span className={'my-auto'}>$65<h6 className={'text-danger'}>$66.95</h6>-$150<h6 className={'text-danger'}>$154.05</h6></span></h4>
+                        <h4 className={'d-flex justify-content-between list-group-item'}>Scalp Treatments<span className={'d-flex'}>$30<h6 className={'text-danger'}>$30.9</h6></span></h4>
                         <h3 className={'text-decoration-underline mt-3 list-group pb-3'}>C O L O R</h3>
-                        <h4 className={'d-flex justify-content-between text-start list-group-item'}>Permanent Single Process W/ Glaze<div className={'my-auto'}>$61-$75</div></h4>
-                        <h4 className={'d-flex justify-content-between list-group-item'}>Semi Permanent<span className={'justify-content-between'}>$55-$65</span></h4>
-                        <h4 className={'d-flex justify-content-between list-group-item'}>Glaze (Alone)<span className={'justify-content-between'}>$30-$40</span></h4>
-                        <h4 className={'d-flex justify-content-between list-group-item'}>Highlites (Full Head)<span className={'justify-content-between my-auto'}>$85-$200</span></h4>
-                        <h4 className={'d-flex justify-content-between list-group-item'}>Highlites (Partial)<span className={'justify-content-between'}>$65-$125</span></h4>
-                        <h4 className={'d-flex justify-content-between list-group-item'}>Hairline Touch-Up<span className={'justify-content-between'}>$36</span></h4>
+                        <h4 className={'d-flex justify-content-between text-start list-group-item'}>Permanent Single Process W/ Glaze<div className={'my-auto'}>$61<h6 className={'text-danger'}>$62.83</h6>-$75<h6 className={'text-danger'}>$77.25</h6></div></h4>
+                        <h4 className={'d-flex justify-content-between list-group-item'}>Semi Permanent<span className={'justify-content-between'}>$55<h6 className={'text-danger'}>$56.65</h6>-$65<h6 className={'text-danger'}>$66.95</h6></span></h4>
+                        <h4 className={'d-flex justify-content-between list-group-item'}>Glaze (Alone)<span className={'justify-content-between'}>$30<h6 className={'text-danger'}>$30.9</h6>-$40<h6 className={'text-danger'}>$41.2</h6></span></h4>
+                        <h4 className={'d-flex justify-content-between list-group-item'}>Highlites (Full Head)<span className={'justify-content-between my-auto'}>$85<h6 className={'text-danger'}>$87.55</h6>-$200<h6 className={'text-danger'}>$206</h6></span></h4>
+                        <h4 className={'d-flex justify-content-between list-group-item'}>Highlites (Partial)<span className={'justify-content-between'}>$65<h6 className={'text-danger'}>$66.95</h6>-$125<h6 className={'text-danger'}>$206</h6></span></h4>
+                        <h4 className={'d-flex justify-content-between list-group-item'}>Hairline Touch-Up<span className={'justify-content-between'}>$36<h6 className={'text-danger'}>$37.08</h6></span></h4>
                     </ul>
                     <ul className={'col-12 col-lg-4 list-group'}><h2 className={'border-bottom'}>Hair Removal</h2>
-                    <h4 className={'d-flex justify-content-between list-group-item'}>Threading<span className={''}>$12-$30</span></h4>
+                    <h4 className={'d-flex justify-content-between list-group-item'}>Threading<span className={'d-flex'}>$12<h6 className={'text-danger'}>$12.36</h6>-$30<h6 className={'text-danger'}>$30.9</h6></span></h4>
                         <h3 className={'text-decoration-underline list-group pb-3'}>F A C E</h3>                        
-                        <h4 className={'d-flex justify-content-between list-group-item'}>Lip Wax<span className={''}>$9</span></h4>
-                        <h4 className={'d-flex justify-content-between list-group-item'}>Chin Wax<span className={''}>$9</span></h4>
-                        <h4 className={'d-flex justify-content-between list-group-item'}>Eyebrow Wax<span className={''}>$10</span></h4>
-                        <h4 className={'d-flex justify-content-between list-group-item'}>Sides of Face Wax<span className={''}>$12</span></h4>
+                        <h4 className={'d-flex justify-content-between list-group-item'}>Lip Wax<span className={'d-flex'}>$9<h6 className={'text-danger'}>$9.27</h6></span></h4>
+                        <h4 className={'d-flex justify-content-between list-group-item'}>Chin Wax<span className={'d-flex'}>$9<h6 className={'text-danger'}>$9.27</h6></span></h4>
+                        <h4 className={'d-flex justify-content-between list-group-item'}>Eyebrow Wax<span className={'d-flex'}>$10<h6 className={'text-danger'}>$10.30</h6></span></h4>
+                        <h4 className={'d-flex justify-content-between list-group-item'}>Sides of Face Wax<span className={'d-flex'}>$12<h6 className={'text-danger'}>$12.36</h6></span></h4>
                         <h3 className={'text-decoration-underline list-group pb-3'}>B O D Y</h3>
-                        <h4 className={'d-flex justify-content-between list-group-item'}>Brazilian<span className={''}>$40</span></h4>
-                        <h4 className={'d-flex justify-content-between list-group-item'}>Half-Leg<span className={''}>$23</span></h4>
-                        <h4 className={'d-flex justify-content-between list-group-item'}>Full-Leg<span className={''}>$45</span></h4>
-                        <h4 className={'d-flex justify-content-between list-group-item'}>Bikini<span className={''}>$18</span></h4>
-                        <h4 className={'d-flex justify-content-between list-group-item'}>Underarm<span className={''}>$15</span></h4>
-                        <h4 className={'d-flex justify-content-between list-group-item'}>Tummy Line<span className={''}>$7-$9</span></h4>
-                        <h4 className={'d-flex justify-content-between list-group-item'}>Half-Arm<span className={''}>$18</span></h4>
-                        <h4 className={'d-flex justify-content-between list-group-item'}>Full-Arm<span className={''}>$36</span></h4>
-                        <h4 className={'d-flex justify-content-between list-group-item'}>Full-Leg W/ Bikini<span className={''}>$50</span></h4>
+                        <h4 className={'d-flex justify-content-between list-group-item'}>Brazilian<span className={'d-flex'}>$40<h6 className={'text-danger'}>$41.20</h6></span></h4>
+                        <h4 className={'d-flex justify-content-between list-group-item'}>Half-Leg<span className={'d-flex'}>$23<h6 className={'text-danger'}>$23.69</h6></span></h4>
+                        <h4 className={'d-flex justify-content-between list-group-item'}>Full-Leg<span className={'d-flex'}>$45<h6 className={'text-danger'}>$46.35</h6></span></h4>
+                        <h4 className={'d-flex justify-content-between list-group-item'}>Bikini<span className={'d-flex'}>$18<h6 className={'text-danger'}>$18.54</h6></span></h4>
+                        <h4 className={'d-flex justify-content-between list-group-item'}>Underarm<span className={'d-flex'}>$15<h6 className={'text-danger'}>$15.45</h6></span></h4>
+                        <h4 className={'d-flex justify-content-between list-group-item'}>Tummy Line<span className={'d-flex'}>$7<h6 className={'text-danger'}>$7.21</h6>-$9<h6 className={'text-danger'}>$9.27</h6></span></h4>
+                        <h4 className={'d-flex justify-content-between list-group-item'}>Half-Arm<span className={'d-flex'}>$18<h6 className={'text-danger'}>$18.54</h6></span></h4>
+                        <h4 className={'d-flex justify-content-between list-group-item'}>Full-Arm<span className={'d-flex'}>$36<h6 className={'text-danger'}>$37.08</h6></span></h4>
+                        <h4 className={'d-flex justify-content-between list-group-item'}>Full-Leg W/ Bikini<span className={'d-flex'}>$50<h6 className={'text-danger'}>$51.50</h6></span></h4>
 
                         <h3 className={'text-decoration-underline text-danger'}>TUESDAY & WEDNESDAY SPECIALS</h3>
-                        <h4 className={'d-flex justify-content-between text-danger list-group-item'}>Full-Leg<span className={''}>$35</span></h4>
-                        <h4 className={'d-flex justify-content-between text-danger list-group-item'}>Full-Leg W/ Bikini<span className={''}>$42</span></h4>
-                        <h4 className={'d-flex justify-content-between text-danger list-group-item'}>Manicure & Pedicure<span className={''}>$30</span></h4>
+                        <h4 className={'d-flex justify-content-between text-danger list-group-item'}>Full-Leg<span className={'d-flex'}>$35<h6 className={'text-danger'}>$36.05</h6></span></h4>
+                        <h4 className={'d-flex justify-content-between text-danger list-group-item'}>Full-Leg W/ Bikini<span className={'d-flex'}>$42<h6 className={'text-danger'}>$43.26</h6></span></h4>
+                        <h4 className={'d-flex justify-content-between text-danger list-group-item'}>Manicure & Pedicure<span className={'d-flex'}>$30<h6 className={'text-danger'}>$30.09</h6></span></h4>
                     </ul>
                     <ul className={'col-12 col-lg-4 list-group'}><h2 className={'border-bottom'}>Manicure & Pedicure</h2>
                         <h3 className={'text-decoration-underline list-group pb-3'}>F I N G E R N A I L S</h3>
-                        <h4 className={'d-flex justify-content-between list-group-item'}>Manicure / French<span className={''}>$15/$18</span></h4>
-                        <h4 className={'d-flex justify-content-between list-group-item'}>Shellac Gel Manicure<span className={''}>$35</span></h4>
-                        <h4 className={'d-flex justify-content-between text-start list-group-item'}>SNS Powder Manicure / French<span className={'my-auto'}>$40/$50</span></h4>
-                        <h4 className={'d-flex justify-content-between list-group-item'}>Powder Fill-In<span className={''}>$32</span></h4>
-                        <h4 className={'d-flex justify-content-between list-group-item'}>Silk Wrap<span className={''}>$50</span></h4>
-                        <h4 className={'d-flex justify-content-between text-start list-group-item'}>Change of Polish<span className={'d-flex justify-content-end w-100'}>Fingers $10 <br></br>Toes $15</span></h4>
-                        <h4 className={'d-flex justify-content-between text-start list-group-item'}>Change of French Polish<span className={'d-flex justify-content-end w-100 my-auto'}>Fingers $13 <br></br>Toes $20</span></h4>
-                        <h4 className={'d-flex justify-content-between list-group-item'}>Full Set with Tips<span className={''}>$50</span></h4>                        
-                        <h4 className={'d-flex justify-content-between text-start list-group-item'}>Glue Down<span className={'d-flex justify-content-end w-100'}>$8 <br/>Single Wrap $5</span></h4>
-                        <h4 className={'d-flex justify-content-between list-group-item'}>Single Tip with Wrap<span className={''}>$8</span></h4>
+                        <h4 className={'d-flex justify-content-between list-group-item'}>Manicure / French<span className={'d-flex'}>$15<h6 className={'text-danger'}>$15.45</h6>/$18<h6 className={'text-danger'}>$18.54</h6></span></h4>
+                        <h4 className={'d-flex justify-content-between list-group-item'}>Shellac Gel Manicure<span className={'d-flex'}>$35<h6 className={'text-danger'}>$36.05</h6></span></h4>
+                        <h4 className={'d-flex justify-content-between text-start list-group-item'}>SNS Powder Manicure / French<span className={'my-auto'}>$40<h6 className={'text-danger'}>$41.20</h6>/$50<h6 className={'text-danger'}>$51.50</h6></span></h4>
+                        <h4 className={'d-flex justify-content-between list-group-item'}>Powder Fill-In<span className={'d-flex'}>$32<h6 className={'text-danger'}>$32.96</h6></span></h4>
+                        <h4 className={'d-flex justify-content-between list-group-item'}>Silk Wrap<span className={'d-flex'}>$50<h6 className={'text-danger'}>$51.50</h6></span></h4>
+                        <h4 className={'d-flex justify-content-between text-start list-group-item'}>Change of Polish<span className={'d-flex justify-content-end w-100'}>Fingers $10<h6 className={'text-danger'}>$10.30</h6> <br></br>Toes $15<h6 className={'text-danger'}>$15.45</h6></span></h4>
+                        <h4 className={'d-flex justify-content-between text-start list-group-item'}>Change of French Polish<span className={'d-flex justify-content-end w-100 my-auto'}>Fingers $13<h6 className={'text-danger'}>$13.39</h6> <br></br>Toes $20<h6 className={'text-danger'}>$20.60</h6></span></h4>
+                        <h4 className={'d-flex justify-content-between list-group-item'}>Full Set with Tips<span className={'d-flex'}>$50<h6 className={'text-danger'}>$51.50</h6></span></h4>                        
+                        <h4 className={'d-flex justify-content-between text-start list-group-item'}>Glue Down<span className={'d-flex justify-content-end w-100'}>$8<h6 className={'text-danger'}>$8.24</h6> <br/>Single Wrap $5<h6 className={'text-danger'}>$5.15</h6></span></h4>
+                        <h4 className={'d-flex justify-content-between list-group-item'}>Single Tip with Wrap<span className={'d-flex'}>$8<h6 className={'text-danger'}>$8.24</h6></span></h4>
                         <h3 className={'text-decoration-underline list-group pb-3'}>S P A <br/>S E R V I C E S</h3>
-                        <h4 className={'d-flex justify-content-between list-group-item'}>Classic Pedicure<span className={''}>$28</span></h4>
+                        <h4 className={'d-flex justify-content-between list-group-item'}>Classic Pedicure<span className={'d-flex'}>$28<h6 className={'text-danger'}>$28.84</h6></span></h4>
                         <p className={'fs-5 my-0 text-decoration-underline'}>30 minutes</p>
                         <p className={'fs-5'}>This treatment includes an Aqua Therapy Soak, lower leg massage, exfoliation, classic nail care & polish</p>
-                        <h4 className={'d-flex justify-content-between list-group-item'}>French Pedicure<span className={''}>$33</span></h4>
+                        <h4 className={'d-flex justify-content-between list-group-item'}>French Pedicure<span className={'d-flex'}>$33<h6 className={'text-danger'}>$33.99</h6></span></h4>
 
-                        <h4 className={'d-flex justify-content-between list-group-item'}>Spa Pedicure<span className={''}>$50</span></h4>
+                        <h4 className={'d-flex justify-content-between list-group-item'}>Spa Pedicure<span className={'d-flex'}>$50<h6 className={'text-danger'}>$51.50</h6></span></h4>
                         <p className={'fs-5 my-0 text-decoration-underline'}>45 minutes</p>
                         <p className={'fs-5 my-0'}>Experience the Natural Benefits of a Sea Rock Soak, Alpha Hydroxy Sea Scrub, Hydrating Marine Mask, and a Hot Oil Massage to finish.</p>
                         <p className={'fs-5 my-0 pt-3'}>Finally, if you can, choose your nail color!</p>
