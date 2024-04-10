e927247b3d55e96df7afa3a10ed730ff0ac50b9c
RMottola68 <RMottola68@gmail.com>
2024-04-10 17:01:41 -0400

    fixed tax prices

--- src/Services.js
+++ src/Services.js
@@ -24,17 +24,17 @@ const Services = (props) => {
                         <h4 className={'d-flex justify-content-between list-group-item'}>Keratin Treatment<span className={'d-flex'}>$103<h6 className={'text-danger'}>$106.09</h6>/HR</span></h4>
                         <h4 className={'d-flex justify-content-between list-group-item'}>Body Wave / Perm <span className={'d-flex'}>$75<h6 className={'text-danger'}>$77.25</h6>-$150<h6 className={'text-danger'}>$154.05</h6></span></h4>
                         <h4 className={'d-flex justify-content-between list-group-item'}>Special Effects & Relaxers<span className={'my-auto d-flex ml-5'}>$65<h6 className={'text-danger'}>$66.95</h6>-$150<h6 className={'text-danger'}>$154.05</h6></span></h4>
-                        <h4 className={'d-flex justify-content-between list-group-item'}>Scalp Treatments<span className={'d-flex'}>$30<h6 className={'text-danger'}>$30.9</h6></span></h4>
+                        <h4 className={'d-flex justify-content-between list-group-item'}>Scalp Treatments<span className={'d-flex'}>$30<h6 className={'text-danger'}>$30.90</h6></span></h4>
                         <h3 className={'text-decoration-underline mt-3 list-group pb-3'}>C O L O R</h3>
                         <h4 className={'d-flex justify-content-between text-start list-group-item'}>Permanent Single Process W/ Glaze<div className={'d-flex ml-5 my-auto'}>$61<h6 className={'text-danger'}>$62.83</h6>-$75<h6 className={'text-danger'}>$77.25</h6></div></h4>
                         <h4 className={'d-flex justify-content-between list-group-item'}>Semi Permanent<span className={'justify-content-between d-flex ml-5'}>$55<h6 className={'text-danger'}>$56.65</h6>-$65<h6 className={'text-danger'}>$66.95</h6></span></h4>
-                        <h4 className={'d-flex justify-content-between list-group-item'}>Glaze (Alone)<span className={'justify-content-between d-flex ml-5'}>$30<h6 className={'text-danger'}>$30.9</h6>-$40<h6 className={'text-danger'}>$41.2</h6></span></h4>
+                        <h4 className={'d-flex justify-content-between list-group-item'}>Glaze (Alone)<span className={'justify-content-between d-flex ml-5'}>$30<h6 className={'text-danger'}>$30.90</h6>-$40<h6 className={'text-danger'}>$41.2</h6></span></h4>
                         <h4 className={'d-flex justify-content-between list-group-item'}>Highlites (Full Head)<span className={'justify-content-between my-auto d-flex ml-5'}>$85<h6 className={'text-danger'}>$87.55</h6>-$200<h6 className={'text-danger'}>$206</h6></span></h4>
-                        <h4 className={'d-flex justify-content-between list-group-item'}>Highlites (Partial)<span className={'justify-content-between d-flex ml-5'}>$65<h6 className={'text-danger'}>$66.95</h6>-$125<h6 className={'text-danger'}>$206</h6></span></h4>
+                        <h4 className={'d-flex justify-content-between list-group-item'}>Highlites (Partial)<span className={'justify-content-between d-flex ml-5'}>$65<h6 className={'text-danger'}>$66.95</h6>-$125<h6 className={'text-danger'}>$128.75</h6></span></h4>
                         <h4 className={'d-flex justify-content-between list-group-item'}>Hairline Touch-Up<span className={'justify-content-between d-flex ml-5'}>$36<h6 className={'text-danger'}>$37.08</h6></span></h4>
                     </ul>
                     <ul className={'col-12 col-lg-6 col-xl-4 list-group'}><h2 className={'border-bottom'}>Hair Removal</h2>
-                    <h4 className={'d-flex justify-content-between list-group-item'}>Threading<span className={'d-flex'}>$12<h6 className={'text-danger'}>$12.36</h6>-$30<h6 className={'text-danger'}>$30.9</h6></span></h4>
+                    <h4 className={'d-flex justify-content-between list-group-item'}>Threading<span className={'d-flex'}>$12<h6 className={'text-danger'}>$12.36</h6>-$30<h6 className={'text-danger'}>$30.90</h6></span></h4>
                         <h3 className={'text-decoration-underline list-group pb-3'}>F A C E</h3>                        
                         <h4 className={'d-flex justify-content-between list-group-item'}>Lip Wax<span className={'d-flex'}>$9<h6 className={'text-danger'}>$9.27</h6></span></h4>
                         <h4 className={'d-flex justify-content-between list-group-item'}>Chin Wax<span className={'d-flex'}>$9<h6 className={'text-danger'}>$9.27</h6></span></h4>
@@ -54,7 +54,7 @@ const Services = (props) => {
                         <h3 className={'text-decoration-underline text-danger'}>TUESDAY & WEDNESDAY SPECIALS</h3>
                         <h4 className={'d-flex justify-content-between text-danger list-group-item'}>Full-Leg<span className={'d-flex'}>$35<h6 className={'text-danger'}>$36.05</h6></span></h4>
                         <h4 className={'d-flex justify-content-between text-danger list-group-item'}>Full-Leg W/ Bikini<span className={'d-flex'}>$42<h6 className={'text-danger'}>$43.26</h6></span></h4>
-                        <h4 className={'d-flex justify-content-between text-danger list-group-item'}>Manicure & Pedicure<span className={'d-flex'}>$30<h6 className={'text-danger'}>$30.09</h6></span></h4>
+                        <h4 className={'d-flex justify-content-between text-danger list-group-item'}>Manicure & Pedicure<span className={'d-flex'}>$30<h6 className={'text-danger'}>$30.90</h6></span></h4>
                     </ul>
                     <ul className={'col-12 col-xl-4 list-group'}><h2 className={'border-bottom'}>Manicure & Pedicure</h2>
                         <h3 className={'text-decoration-underline list-group pb-3'}>F I N G E R N A I L S</h3>
